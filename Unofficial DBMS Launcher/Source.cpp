bcbb5a312701b2ce1487baeec2ced5d43a659cfa
danielharton <hartondaniel11@gmail.com>
2025-06-26 22:50:41 +0300

    Update first window

--- Unofficial DBMS Launcher/Source.cpp
+++ Unofficial DBMS Launcher/Source.cpp
@@ -29,7 +29,8 @@ std::wstring SERVER_PATH;
 // License text
 const wchar_t* LICENSE_TEXT =
 L"Unofficial Oracle server management software. Not affiliated with Oracle Corporation.\r\n"
-L"Copyright 2025 FirstName LastName\r\n\r\n"
+L"https://github.com/danielharton/Unofficial-DBMS-Launcher\r\n"
+L"Copyright 2025 Daniel Harton\r\n\r\n"
 L"Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:\r\n\r\n"
 L"The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\r\n\r\n"
 L"THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.";
